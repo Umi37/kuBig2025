306caca7550356dbceb2a7ca096b7d4625cacb54
Umi37 <rimshot99@naver.com>
2025-03-05 14:07:49 +0900

    Day2 - 2

--- c_example/limit.c
+++ c_example/limit.c
@@ -1,10 +1,18 @@
 #include <limits.h>
 #include <stdio.h> //standard input, output drag F12
 
-int main()
+int main(void)
 {
-    printf("hello,world!\n");
+    printHello();
     printf("char_bit : %d\n", CHAR_BIT);
     printf("char_max : %d\n", CHAR_MAX);
+    printf("INT_MIN : %d \n INT_MAX : %d\n", INT_MIN, INT_MAX);
+    printf("LONG_MIN : %ld \n LONG_MAX : %ld\n", LONG_MIN, LONG_MAX);
+    printf("INT_MAX + 1 : %d\n", INT_MAX + 1);
     return 0;
+}
+
+void printHello()
+{
+    printf("hello, world\n");
 }
