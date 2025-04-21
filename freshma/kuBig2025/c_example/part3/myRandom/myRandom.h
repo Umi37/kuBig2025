a870f0f6525a7481b641a630ab5f54fa999b2b39
Umi <rimshot99@naver.com>
2025-04-21 20:55:32 +0900

    stl

--- freshma/kuBig2025/c_example/part3/myRandom/myRandom.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void my_srand(int s);
-int my_rand(void);
--- stl/ex6_iterator.cpp
+++ stl/ex6_iterator.cpp
@@ -17,7 +17,17 @@ int main()
 
     // iterator 가 정의 - begin()과 end()를 통해서 반복자 생성
     // *it 역참조가 가능해야 한다.
-    for (auto v : test_vector) // for 문 세번째 방식
+    for (auto v : test_vector) // 세번째 방식 모던 C++ 문법
+    {
+        cout << v << endl;
+    }
+
+    for (auto &v : test_vector) // 컨테이너의 값을 변경
+    {
+        cout << v << endl;
+    }
+
+    for (const auto &v : test_vector) // 메모리 효율적으로 값 참고 할 때
     {
         cout << v << endl;
     }
