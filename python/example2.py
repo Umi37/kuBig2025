77ca52c0ea5c5de93e0dc6c503ec0fb30fb0730b
Umi <rimshot99@naver.com>
2025-05-07 16:54:30 +0900

    Add: 입력값이 숫자인지 확인하고, 올바른 입력이 들어올 때까지 반복하도록 수정

--- python/example2.py
+++ python/example2.py
@@ -1,8 +1,15 @@
 # 섭씨 -> 화씨 변환 프로그램
 
 def main():
-    # 섭씨 온도 입력
-    celsius = float(input("섭씨 온도를 입력하세요: "))
+    while True:
+        # 섭씨 온도 입력
+        celsius = input("섭씨 온도를 입력하세요: ")
+        # 입력값이 숫자인지 확인
+        try: 
+            celsius = float(celsius)
+            break # 성공적으로 변환되면 반복 종료
+        except ValueError:
+            print("유효하지 않은 입력입니다. 숫자를 입력하세요.: ")
 
     # 화씨 온도로 변환
     fahrenheit = (celsius * 9/5) + 32
